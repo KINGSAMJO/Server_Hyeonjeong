6d577d8c972801271d66fe47fe11eabacf8b8ae1
hyeonjeong Park <py0429@ewhain.net>
2022-04-08 13:36:09 +0900

    1-2 회원가입 페이

--- seminar_task1/app/src/main/java/com/example/seminar_task1/SignUpActivity.kt
+++ seminar_task1/app/src/main/java/com/example/seminar_task1/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.example.seminar_task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.seminar_task1.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -12,6 +13,23 @@ class SignUpActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        binding.btnSignupEnd.setOnClickListener {
+            signup()
+        }
+
+    }
+
+    private fun signup(){
+        if(binding.etSignupId.text.toString().isNotEmpty() && binding.etSignupName.text.toString().isNotEmpty() && binding.etSignupPw.text.toString().isNotEmpty()){
+            Toast.makeText(this,"회원가입성공", Toast.LENGTH_SHORT).show()
+            finish()
+        }else{
+            Toast.makeText(this,"입력되지 않은 정보가 있습니다",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun finish() {
+        super.finish()
     }
 
 }
