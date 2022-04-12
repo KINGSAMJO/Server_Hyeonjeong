f630c31a9512b71fda57b238fb3bbb056bd571f5
hyeonjeong Park <py0429@ewhain.net>
2022-04-12 13:56:52 +0900

    reflect codereview

--- seminar_task1/app/src/main/java/com/example/seminar_task1/SignInActivity.kt
+++ seminar_task1/app/src/main/java/com/example/seminar_task1/SignInActivity.kt
@@ -25,6 +25,27 @@ class SignInActivity : AppCompatActivity() {
         etpw = binding.etPw
 
         //로그인 버튼 클릭시
+        signIn()
+
+        //회원가입 버튼 클릭시
+        signUp()
+
+        //회원가입 후 ID, PW 가져오기
+        signUp()
+        getSignUpActivityResult = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            result ->
+                //회원가입에서 돌아올 때 결과 값 받아옴
+                if(result.resultCode == RESULT_OK){
+                    val signupId = result.data?.getStringExtra("id")
+                    val signupPw = result.data?.getStringExtra("pw")
+                    etid.setText(signupId)
+                    etpw.setText(signupPw)
+                }
+            }
+    }
+
+    private fun signIn(){
         binding.btnLogin.setOnClickListener {
             if(etid.text.toString().isNotBlank() && etpw.text.toString().isNotBlank()){//값이 있는 경우
                 Toast.makeText(this,"로그인 성공",Toast.LENGTH_SHORT).show()
@@ -34,25 +55,14 @@ class SignInActivity : AppCompatActivity() {
                 Toast.makeText(this,"아이디/비밀번호를 확인해주세요",Toast.LENGTH_SHORT).show()
             }
         }
+    }
 
-        //회원가입 버튼 클릭시
+    private fun signUp(){
         binding.btnSignup.setOnClickListener {
             val intent = Intent(this,SignUpActivity::class.java)
             getSignUpActivityResult.launch(intent) //startActivity 대신 사용해서 값 받아올 수 있도록 함
         }
 
-        //회원가입 후 ID, PW 가져오기
-        getSignUpActivityResult = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()) {
-            result ->
-                //회원가입에서 돌아올 때 결과 값 받아옴
-                if(result.resultCode == RESULT_OK){
-                    var signupId = result.data?.getStringExtra("id")
-                    var signupPw = result.data?.getStringExtra("pw")
-                    etid.setText(signupId)
-                    etpw.setText(signupPw)
-                }
-            }
     }
 
 }
--- seminar_task1/app/src/main/java/com/example/seminar_task1/SignUpActivity.kt
+++ seminar_task1/app/src/main/java/com/example/seminar_task1/SignUpActivity.kt
@@ -15,32 +15,34 @@ class SignUpActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        binding.btnSignupEnd.setOnClickListener {
-            signup()
-        }
+        signUp()
+
 
     }
 
     //회원가입 완료 버튼 클릭시
-    private fun signup(){
-        if(binding.etSignupId.text.toString().isNotBlank() && binding.etSignupName.text.toString().isNotBlank() && binding.etSignupPw.text.toString().isNotEmpty()){
-            Toast.makeText(this,"회원가입성공", Toast.LENGTH_SHORT).show()
-            var signupId : String = binding.etSignupId.text.toString()
-            var signupPw : String = binding.etSignupPw.text.toString()
+    private fun signUp(){
+        binding.btnSignupEnd.setOnClickListener{
+            if(binding.etSignupId.text.toString().isNotBlank() && binding.etSignupName.text.toString().isNotBlank() && binding.etSignupPw.text.toString().isNotEmpty()){
+                Toast.makeText(this,"회원가입성공", Toast.LENGTH_SHORT).show()
+                var signupId : String = binding.etSignupId.text.toString()
+                var signupPw : String = binding.etSignupPw.text.toString()
 
-            //로그인 페이지로 이동할 때 입력 값을 보내도록 하는 코드
-            val intent = Intent(this, SignInActivity::class.java).apply{
-                putExtra("id",signupId)
-                putExtra("pw",signupPw)
-            }
+                //로그인 페이지로 이동할 때 입력 값을 보내도록 하는 코드
+                val intent = Intent(this, SignInActivity::class.java).apply{
+                    putExtra("id",signupId)
+                    putExtra("pw",signupPw)
+                }
 
-            setResult(RESULT_OK,intent)
-            if (!isFinishing) {
-                finish()
+                setResult(RESULT_OK,intent)
+                if (!isFinishing) {
+                    finish()
+                }
+            }else{
+                Toast.makeText(this,"입력되지 않은 정보가 있습니다",Toast.LENGTH_SHORT).show()
             }
-        }else{
-            Toast.makeText(this,"입력되지 않은 정보가 있습니다",Toast.LENGTH_SHORT).show()
         }
+
     }
 
     override fun finish() {
