3aae544015917ae5753501bebc4bb281977719c0
hyeonjeong Park <py0429@ewhain.net>
2022-06-12 17:38:45 +0900

    [seminar 7] room 적용하여 autoLogin

--- app/src/main/java/com/example/seminar_task1/model/RequestSignIn.kt
+++ app/src/main/java/com/example/seminar_task1/model/RequestSignIn.kt
@@ -2,8 +2,12 @@ package com.example.seminar_task1.model
 
 import com.google.gson.annotations.SerializedName
 
+
 data class RequestSignIn(
     @SerializedName("email")
     val id:String,
-    val password:String
+    val password:String,
+
+    /*@PrimaryKey(autoGenerate = true)
+    val key : Int*/
 )
