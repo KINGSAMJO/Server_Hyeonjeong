1735041a50a597437102b8a1508c6602eceb3d87
hyeonjeong Park <py0429@ewhain.net>
2022-06-16 23:15:10 +0900

    #7 [refactor] githubResponse 생성

--- app/src/main/java/com/example/seminar_task1/data/model/response/ResponseGithub.kt
+++
@@ -1,5 +0,0 @@
-package com.example.seminar_task1.data.model.response
-
-data class ResponseGithub(
-
-)
--- app/src/main/java/com/example/seminar_task1/data/service/GithubService.kt
+++ app/src/main/java/com/example/seminar_task1/data/service/GithubService.kt
@@ -1,6 +1,7 @@
 package com.example.seminar_task1.data.service
 
-import com.example.seminar_task1.data.model.response.ResponseGithub
+import com.example.seminar_task1.data.model.response.ResponseGithubFollowersItem
+import com.example.seminar_task1.data.model.response.ResponseGithubUserId
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -8,11 +9,11 @@ interface GithubService {
     @GET("/users/{userId}")
     suspend fun getUserId(
         @Path("userId") userId : String
-    ): ResponseGithub
+    ): ResponseGithubUserId
 
     @GET("/users/{userId}/followers")
     suspend fun getUserFollowers(
         @Path("userId") userId : String
-    ): ResponseGithub
-    
+    ): List<ResponseGithubFollowersItem>
+
 }
